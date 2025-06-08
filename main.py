b63afe6e2c1a721e2c9478fdcae95771da36d7be
Emil <srcs.karudo@gmail.com>
2025-06-08 11:42:29 +0300

    Update main.py
    Калькулятор с выбором действия

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
+def add(a, b):
+    return a + b
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+def subtract(a, b):
+    return a - b
 
-def sum(a, b):
-    return (a + b)
+def multiply(a, b):
+    return a * b
 
-a = int(input('Введите 1-е число: '))
-b = int(input('Введите 2-е число: '))
+def divide(a, b):
+    if b == 0:
+        return "Ошибка: деление на ноль!"
+    return a / b
 
-print(f'Сумма {a} и {b} равна {sum(a, b)}')
+print("Простой калькулятор на Python")
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
+
+choice = input("Введите номер операции (1/2/3/4): ")
+
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+if choice == "1":
+    result = add(a, b)
+elif choice == "2":
+    result = subtract(a, b)
+elif choice == "3":
+    result = multiply(a, b)
+elif choice == "4":
+    result = divide(a, b)
+else:
+    result = "Ошибка: неверный выбор операции"
+
+print("Результат:", result)
